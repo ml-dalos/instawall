8697797724656535353d359ff017a3a080dbf10d
Alexey Lebedev <joker.leha98@gmail.com>
2020-05-03 08:18:33 +0300

    add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level.to_s
+    when 'notice' then 'info'
+    when 'success' then 'success'
+    when 'error' then 'danger'
+    when 'alert' then 'warning'
+    else 'dark'
+    end
+  end
 end
