c9984468b5b816bdcb002066e4d5062194104313
Alexey Lebedev <joker.leha98@gmail.com>
2020-05-03 11:10:18 +0300

    change navbar for signed in account

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
 module ApplicationHelper
   def flash_class(level)
-    puts '----------------'
-    puts level.to_s
     case level.to_s
     when 'notice' then 'info'
     when 'success' then 'success'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  validates :image, presence: true
   belongs_to :account
   mount_uploader :image, ImageUploader
 
